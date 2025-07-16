2d27426069aeecb9d942b3728e3ec4e834458303
brad.wu <brad.wu@ebaotech.com>
2025-07-16 18:15:56 +0800

    init

--- groovy/src/lib/captors/decl/annotation.ts
+++ groovy/src/lib/captors/decl/annotation.ts
@@ -20,47 +20,42 @@ export const AnnotationCaptorDefs: GroovyTokenCaptorDefs = {
 	Identifier: {
 		patterns: PtnId,
 		forks: [
-			{
-				forStates: [Incl, S.AnnDeclSt, S.AnnDeclDotEd],
-				collect: [SS, S.AnnDeclIdEd]
-			},
-			{
-				forStates: [Incl, S.AnnDeclValsSt, S.AnnDeclValsCommaEd],
-				collect: [CB, T.AnnotationDeclValue, S.AnnDeclValIdEd]
-			},
+			// appears after start mark, dot
+			{forStates: [Incl, S.AnnDeclSt, S.AnnDeclDotEd], collect: [SS, S.AnnDeclIdEd]},
+			// when current state is any of annotation values, create annotation value
+			{forStates: [Incl, SG.AnnVals], collect: [CB, T.AnnotationDeclValue, S.AnnDeclValIdEd]},
+			// when current state is annotation value started, transit to identifier end
 			{forStates: [Incl, S.AnnDeclValSt], collect: [SS, S.AnnDeclValIdEd]}
 		]
 	},
-	Dot: {
+	Dot: { // appears only after annotation identifier
 		patterns: '.',
 		forStates: [Incl, S.AnnDeclIdEd],
 		collect: [SS, S.AnnDeclDotEd]
+		// TODO annotation value also could be qualified name, which is a class
 	},
 	Assign: {
 		patterns: '=',
 		forks: [
-			{
-				forStates: [Incl, S.AnnDeclValsSt, S.AnnDeclValsCommaEd],
-				collect: [CB, T.AnnotationDeclValue, S.AnnDeclValEqEd]
-			},
+			// when current state is any of annotation values, create annotation value
+			{forStates: [Incl, SG.AnnVals], collect: [CB, T.AnnotationDeclValue, S.AnnDeclValEqEd]},
+			// when current state is any of annotation value, transit to equal end
 			{forStates: [Incl, SG.AnnVal], collect: [SS, S.AnnDeclValEqEd]}
 		]
 	},
 	Comma: {
 		patterns: ',',
 		forks: [
-			{
-				forStates: [Incl, S.AnnDeclValsSt, S.AnnDeclValsCommaEd],
-				collect: [SS, S.AnnDeclValsCommaEd]
-			},
-			{
+			// when current state is any of annotation values, transit to comma end
+			{forStates: [Incl, SG.AnnVals], collect: [SS, S.AnnDeclValsCommaEd]},
+			{ // when current state is any of annotation value, end it
 				forStates: [Incl, SG.AnnVal],
 				beforeCollect: EBBC,
 				collect: [SS, S.AnnDeclValsCommaEd]
 			}
 		]
 	},
-	LParen: {
+	LParen: { // appears only after annotation identifier
 		patterns: '(',
 		forStates: [Incl, S.AnnDeclIdEd],
 		collect: [CB, T.AnnotationDeclValues, S.AnnDeclValsSt]
