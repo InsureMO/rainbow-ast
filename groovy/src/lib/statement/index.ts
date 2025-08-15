b100fc5c6966a61b4b528cf759f5d182932a33f4
brad.wu <brad.wu@ebaotech.com>
2025-08-15 16:13:52 +0800

    init

--- groovy/src/lib/statement/index.ts
+++ groovy/src/lib/statement/index.ts
@@ -5,8 +5,8 @@
 // 🆘 do-while -> keyword
 // 🆘 for -> keyword
 // 🆘 if-else -> keyword
-// 🆘 label -> identifier: statement
-// 🆘 variable -> tsscmfv, partial done
+// 🆘 label -> identifier: statement?
+// 🆘 variable declaration -> tsscmfv, partial done
 // 🆘 return -> keyword
 // ✅ semicolon -> common
 // ✅ switch-case -> keyword
@@ -29,3 +29,14 @@
 //
 // parens/block completes method call
 // index makes method call to property get with index
+
+/**
+ * 1. after keywords assert/return/throw/yield, the groovy dsl (using whitespace instead of dot) is not supported,
+ *    unless they are within a single parenthesized block.
+ * 2. to distinguish between a field/variable declaration and an expression:
+ *    - A[.B[...[.N]]] C[, D[...[, N]]] -> field/variable declaration
+ *    - A[.B[...[.N]]] C[, D[...[, N]]] E -> expression
+ *    the difference is:
+ *    - a field/variable declaration has only one whitespace (whitespace around commas is not counted),
+ *    - an expression has at least two whitespaces.
+ */
